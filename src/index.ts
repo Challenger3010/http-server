caded4b3eaa7389c8fad58843e88146026a7d076
Nick <hoernernick@web.de>
2026-03-24 14:12:07 +0100

    added a middleware to log error requests

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
-import { handlerReadiness } from "./api/readiness";
+import { handlerReadiness } from "./api/readiness.js";
+import { middlewareLogResponse } from "./middleware/middlewareLogResponse.js";
 
 const app = express();
 const PORT = 8080;
 
 app.use("/app", express.static("./src/app"));
+app.use(middlewareLogResponse);
 
 app.get("/healthz", handlerReadiness);
 
