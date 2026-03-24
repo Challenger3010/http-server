df5ffff30a22dd4f47c6a31fabb84f9bfb00782d
Nick <hoernernick@web.de>
2026-03-24 15:41:08 +0100

    added admin route

---
+++ src/api/metrics.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import { config } from "../config.js";
+
+export async function handlerMetrics(req: Request, res: Response) {
+  res.set("Content-Type", "text/html; charset=utf-8");
+  res.send(`<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited ${config.fileserverHits} times!</p>
+  </body>
+</html>`);
+}
+
+export async function handlerResetMetrics(req: Request, res: Response) {
+  config.fileserverHits = 0;
+  res.write("Hits reset to 0");
+  res.end();
+}
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import express from "express";
 import { handlerReadiness } from "./api/readiness.js";
 import { middlewareLogResponse } from "./middleware/middlewareLogResponse.js";
+import { middlewareMetricsInc } from "./middleware/metricsInc.js";
+import { handlerMetrics, handlerResetMetrics } from "./api/metrics.js";
 
 const app = express();
 const PORT = 8080;
 
-app.use("/app", express.static("./src/app"));
+app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 app.use(middlewareLogResponse);
 
-app.get("/healthz", handlerReadiness);
+app.get("/admin/metrics", handlerMetrics);
+app.get("/admin/reset", handlerResetMetrics);
+app.get("/api/healthz", handlerReadiness);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
